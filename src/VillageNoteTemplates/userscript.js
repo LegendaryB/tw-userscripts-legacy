7f5952a9dedd4165496dc90f709edc19b8005622
LegendaryB <6391022+LegendaryB@users.noreply.github.com>
2022-07-26 20:21:22 +0200

    Removed userscript header

--- src/VillageNoteTemplates/userscript.js
+++ src/VillageNoteTemplates/userscript.js
@@ -1,16 +1,3 @@
-// ==UserScript==
-// @name         Village note templates
-// @namespace    https://github.com/LegendaryB/tw-userscripts
-// @version      0.1
-// @author       LegendaryB
-// @description  
-// @include      https://de*.die-staemme.de/game.php?*&screen=info_village*
-// @include      https://de*.die-staemme.de/game.php?*&screen=settings
-// @include      https://de*.die-staemme.de/game.php?*&screen=settings*mode=settings*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=die-staemme.de
-// @grant        none
-// ==/UserScript==
-
 import VillageNoteTemplate from "./villageNoteTemplate";
 
 // const SCRIPT_ROOT = 'https://raw.githubusercontent.com/LegendaryB/tw-userscripts/main/src/VillageNoteTemplates';
